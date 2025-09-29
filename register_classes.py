0936d53d6e66165370fb5813ef57492f2866a31c
wwlyn <wwlyn@outlook.com>
2025-09-29 19:46:31 -0400

    Change register_classes to userlib.

--- register_classes.py
+++ register_classes.py
@@ -3,6 +3,6 @@ from labscript_devices import register_classes
 # Use a unique name to avoid clashing with any built-in device of the same name
 register_classes(
     'red_pitaya_pyrpl_pid',
-    BLACS_tab='labscript_devices.red_pitaya_pyrpl_pid.blacs_tabs.red_pitaya_pyrpl_pid_tab',
+    BLACS_tab='Cesium.userlib.red_pitaya_pyrpl_pid.blacs_tabs.red_pitaya_pyrpl_pid_tab',
     runviewer_parser=None,
 )
